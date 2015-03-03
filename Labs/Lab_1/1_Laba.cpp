b39369ccc5ceaa8de78246447a5b06f861caa7c2
Anton Davedenko <davedenko83@gmail.com>
2015-03-03 20:12:00 +0300

    add comments

--- Labs/Lab_1/1_Laba.cpp
+++ Labs/Lab_1/1_Laba.cpp
@@ -16,7 +16,10 @@ namespace Laba_1 {
             char[] CharArr = new char[0];
             string Line;
             
-            // 1-------------------------------------
+            /*Задание 1. Даны целые положительные числа А и В (A < B). Вывести все целые числа от А до В включительно;
+            каждое число должно выводиться на новой строке; при этом каждое число должно выводиться количество раз,
+            равное его значению (например, число 3 выводится 3 раза)*/
+            
             Console.Write ( "--- 1 ---\nEnter value of   A: " );
             A = int.Parse ( Console.ReadLine ( ) );// парсим значение в инт
             Console.Write ( "Enter value of B>A: " );
@@ -32,7 +35,14 @@ namespace Laba_1 {
                 }
             } else { Console.WriteLine ( "Wrong value of B" ); }// если А больше В
             
-            // 2-------------------------------------
+            /*Задание 2.Вывести следующие строки (количество строк вводится с клавиатуры ):
+                            A B C D E
+                            B C D E F
+                            C D E F G
+                            D E F G H
+                            E F G H I и т.д.
+                            (Использовать тип char).*/
+
             int numOfSigns = 4;
             Console.Write ( "\n--- 2 ---\nEnter num of strings: " );
             A = int.Parse ( Console.ReadLine ( ) );
@@ -53,7 +63,7 @@ namespace Laba_1 {
                 }
             }
             
-            // 3-------------------------------------
+            //Задание 3. Определить, является ли введенное число числом-перевертышем.
             Console.Write ( "\n--- 3 ---\nEnter value >10: " );
             Line = Console.ReadLine ( );
             CharArr = new char[Line.Length];
@@ -77,7 +87,8 @@ namespace Laba_1 {
                 } count++; 
             }
 
-             // 4-------------------------------------
+             //Задание 4. Проверка на  интервал с дополнительным условием
+             //	'n' – 'e’	X кратно 7
              Console.WriteLine ( "\n--- 4 ---\n" );
              A = 'e'; B = 'n';
              Console.WriteLine ( "'e'={0} 'n'={1}",A,B );
@@ -87,7 +98,8 @@ namespace Laba_1 {
                  }
              }
             
-             // 5-------------------------------------
+             //Оператор switch. Проверка на попадание в список значений
+             //-3,6,99
              Console.Write ( "\n--- 5 ---\nEnter value -3, 6 or 99: " );
              A = int.Parse ( Console.ReadLine ( ) );// парсим значение в инт
                switch ( A ) {
@@ -105,7 +117,9 @@ namespace Laba_1 {
                    break;
                }
            
-           // 6-------------------------------------
+           //Задание 6. Операторы циклов (использовать  операторы while, do-while и for )
+           //Вывести значение выражения в интервале
+           //X*10	-5..5	11
             Console.WriteLine ( "\n--- 6 ---\nRange from -5 to 5" );
             A = -5; B = 5;
             for ( int i = A; i < B; i++ ) {
@@ -115,7 +129,9 @@ namespace Laba_1 {
             }
              
           
-            // 7-------------------------------------
+            // Задание 7
+            //Определить сумму наименьшей и наибольшей цифры в числе
+
             Console.Write ( "\n--- 7 ---\nEnter number value: " );
             Line = Console.ReadLine ( );
             A = int.Parse ( Line );
@@ -130,7 +146,9 @@ namespace Laba_1 {
                 Console.WriteLine ( "min {0} + max {1} = {2}", count, count2, count + count2 );
             } else { Console.WriteLine ( "max {0} + 0 = {0}",A,A  ); }
            
-           // 8-------------------------------------
+           // Задание 8
+           //Рассчитать сумму чисел 100 Фибоначчи : 1, 1, 2, 3, 5, 8 ..   Xn=X(n-1)+X(n-2)
+
             Console.Write ( "\n--- 8 ---\n\nSum of 100 Fibonachi = " );
             decimal a=0;
             decimal b = 1;
@@ -139,10 +157,7 @@ namespace Laba_1 {
                 c = a + b; a = b; b = c;
             }
             Console.WriteLine ("{0}\n\n", b.ToString ( ) );
-           /*
-           // 9-------------------------------------
-            Console.WriteLine ( "\n--- 9 ---\nEnter num of strings: " );
-              */
+           
         }
     }
 }
