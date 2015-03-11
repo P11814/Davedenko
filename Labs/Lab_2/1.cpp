4af239826c1423b6f7b853d3f60bcce1737fb986
Anton Davedenko <davedenko83@gmail.com>
2015-03-11 14:51:15 +0300

    ready

--- Labs/Lab_2/1.cpp
+++ Labs/Lab_2/1.cpp
@@ -1,2 +1,52 @@
-//not ready
-desrdrtger
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Laba_2 {
+    class Program {
+        static void Main ( string[] args ) {
+
+            /*     Задание 1.
+                   Дана строка текста, в которой слова разделены запятыми. Необходимо:
+                              - определить количество слов в строке;
+                              - определить, сколько слов в строке имеют максимальную длину;
+                              - определить, есть ли в данной строке повторяющиеся слова, и вывести их на экран.
+            */
+
+            string str = "be,hair,help,maid,because,is,hair,she,is,jealous";
+            Console.WriteLine ( str );
+            Console.WriteLine ( "--------------------------------------------" );
+            string[] SplitStr = str.Split ( new Char[] { ' ', ',', '.', ':', '!', '?', '\t' } );//забиваем слово в массив, если встречается орпеделенный символ
+            Console.Write ( "Всего {0} слов(а) в строке\n", SplitStr.Length );
+            int MaxWordLength = 0;
+            foreach ( string s in SplitStr ) {
+                if ( s.Length > MaxWordLength ) { MaxWordLength = s.Length; }
+            }
+            int countMax = 0;
+            foreach ( string s in SplitStr ) {
+                if ( s.Length == MaxWordLength ) { countMax++; }
+            }
+            Console.Write ( "Всего {0} слов(а) имеют максимальную длинну\n", countMax );
+
+            Console.Write ( "Повторяющиеся слова:\n" );
+            int StrLength = SplitStr.Length;
+            int result = 0;
+            countMax = 0;
+            for ( int i = 0; i < SplitStr.Length; i++ ) {
+                for ( int j = 0; j < SplitStr.Length; j++ ) {
+                    result = String.CompareOrdinal ( SplitStr[i], SplitStr[j] );
+                    if ( result == 0 ) {
+                        countMax++;
+                        if ( countMax == 2 ) {
+                            Console.WriteLine ( "{0}", SplitStr[i] );
+                        }
+                    }
+                } //countMax = 0;
+            } 
+
+        }
+    }
+}
