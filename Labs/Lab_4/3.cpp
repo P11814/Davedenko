123cfaced8560d3862f0eb6e8ece2453c1a89306
Anton Davedenko <davedenko83@gmail.com>
2015-03-12 13:49:17 +0300

    1

--- Labs/Lab_4/3.cpp
+++ Labs/Lab_4/3.cpp
@@ -13,14 +13,14 @@ namespace ConsoleApplication3 {
 
     class Program {
         static void Main(string [] args) {
-            int ok=0;
-            int a1=0,b1=0,a2=0,b2=0,x=0,y=0;
-            int tmp=0;
+            int ok = 0;
+            double a1 = 0, b1 = 0, a2 = 0, b2 = 0, x = 0, y = 0;
+            double tmp1 = 0, tmp2 = 0;
             Console.WriteLine (@"A1*x + B1*y = 0
 A2*x + B2*y = 0");
 
 
-            while(ok!=1){
+            while (ok != 1) {
                 try {
 
                     Console.Write ("Enter value for A1>0: ");
@@ -36,15 +36,22 @@ A2*x + B2*y = 0");
                 catch (FormatException) {
                     Console.WriteLine ("--------------\nWrong value!\n--------------");
                 }
-            } 
+            }
 
             //5x+8y=0
             //7x+10y=0
             while (ok != 0) {
                 x++;
-                tmp = a1 * x + b1;
-                y = tmp;
-                tmp = a1 * x + b1 * y;
+                tmp1 = b1 - ( a1 * x );// y
+                tmp2 = b2 - ( a2 * x );//y
+                if (tmp1 == tmp2) {
+                Console.WriteLine("x={0} y={1}",x,tmp2);
+                ok = 0;
+                }
+                if (x==1000) {
+                    Console.WriteLine ("No results for both", x, tmp2);
+                    ok = 0;
+                }
             }
 
 
