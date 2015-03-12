1e558694ade5613bc218df0d04112118541ab94d
Anton Davedenko <davedenko83@gmail.com>
2015-03-12 22:30:13 +0300

    готово кроме сортировки

--- Labs/Lab_6/1.cpp
+++ Labs/Lab_6/1.cpp
@@ -15,7 +15,6 @@ using System.Threading.Tasks;
 
 */
 
-//xcv
 
 namespace Laba_6 {
 
@@ -26,12 +25,20 @@ namespace Laba_6 {
         string name;
         int num;
         int price;
+        int numPlaceOfProd;
 
-        public Product(string nam, int num, int price) {
+        public Product ( string nam, int num, int price ) {
             this.name = nam;
             this.num = num;
             this.price = price;
         }
+        
+
+
+
+
+
+
 
         public string NAME {
             set {
@@ -70,6 +77,17 @@ namespace Laba_6 {
                 return price;
             }
         }
+        public int NUMPLACEOFPRODUCT {
+            set {
+                if (value > 0)
+                    numPlaceOfProd = value;
+                else
+                    Console.WriteLine ("Wrong value");
+            }
+            get {
+                return numPlaceOfProd;
+            }
+        }
 
     }// end class Product
 
@@ -79,49 +97,73 @@ namespace Laba_6 {
 
 
     public class Storage {
-        static int numOfProduct = 0;
-        Product [] product;
-        public Storage(int num) {
+        static int numOfProduct = 0;// count of products
+        static int sizeOfStorage=0;
+        
+        Product [] product;// array of products
+        public Storage ( int num ) {
+            sizeOfStorage=num;
             product = new Product [num];
-        }
+            
+        }// end constructor Storage
 
-        public void addProduct(string nam, int num, int price) {
+        
+            
 
-            product [numOfProduct] = new Product (nam, num, price);
-            numOfProduct++;
-        }
+        public void addProduct ( string nam, int num, int price ) {
+            if (numOfProduct!=sizeOfStorage) {
+                product [numOfProduct] = new Product (nam, num, price);
+                numOfProduct++;
+                product [numOfProduct-1].NUMPLACEOFPRODUCT=numOfProduct;
+                
+            } else {
+                Console.WriteLine ("Склад заполнен");
+            }
+        }// end addProduct
 
-        public void findProdByName(string nam) {
 
-            for (int i = 0 ; i < numOfProduct ; i++) {
+        public void findProductByNumber ( int number ) {
+            for (int i = 0; i < numOfProduct; i++) {
+                if (product [i].NUMPLACEOFPRODUCT == number) {
+                    Console.WriteLine (@"
+Name:   {0}
+Number: {1} шт.
+Price:  {2}  p.
+---------------
+               ", product [i].NAME, product [i].NUM, product [i].PRICE);
+                }
+            }
+            if (product [numOfProduct-1].NUMPLACEOFPRODUCT< number) {
+                Console.WriteLine ("-------------------------------------\nThere is no product with this number.\n-------------------------------------\n");
+            }                      
+        } // end findProductByNumber
+
+
+        public void findProdByName ( string nam ) {
+            for (int i = 0; i < numOfProduct; i++) {
                 if (product [i].NAME == nam) {
                     Console.WriteLine (@"
 Name:   {0}
 Number: {1} шт.
 Price:  {2}  p.
----------------", product [i].NAME, product [i].NUM, product [i].PRICE);
-                } 
+---------------
+               ", product [i].NAME, product [i].NUM, product [i].PRICE);
+                }
             }
             if (product [numOfProduct - 1].NAME != nam) {
-                Console.WriteLine ("There is no product with this name.");
+                Console.WriteLine ("-------------------------------------\nThere is no product with this name.\n-------------------------------------\n");
             }
+        }// end findProdByName
 
-
-        }
 
     }// end class Storage
 
 
 
 
-
-
-
-
-
     class Program {
-        static void Main(string [] args) {
-
+        static void Main ( string [] args ) {
+            
             Storage prod = new Storage (100);
             prod.addProduct ("apple", 20, 45);
             prod.addProduct ("bred", 200, 15);
@@ -129,24 +171,25 @@ Price:  {2}  p.
             prod.addProduct ("potato", 235, 25);
             string str;
             int numsearch = 0;
-            Console.WriteLine ("Ведите номер товара, котрый вы хотите найти: ");
+
+            Console.Write ("Ведите номер товара, который вы хотите найти: ");
             while (numsearch == 0) {
                 try {
-                    numsearch = int.Parse (Console.ReadLine ( ));
-                }
-                catch (FormatException){
+                    numsearch = int.Parse (Console.ReadLine ());
+                } catch (FormatException) {
                     Console.WriteLine ("Введите число");
                 }
 
             }
-            Console.WriteLine ("Ведите наименование товара, котрый вы хотите найти: ");
-            str = Console.ReadLine ( );
+            prod.findProductByNumber (numsearch);
+
+
+            Console.Write ("Ведите наименование товара, который вы хотите найти: ");
+            str = Console.ReadLine ();
             prod.findProdByName (str);
 
             
 
-
         }
     }
 }
-
