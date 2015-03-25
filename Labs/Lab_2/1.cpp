852047512b699ae111194afdcd62abdcbfb88825
Anton Davedenko <davedenko83@gmail.com>
2015-03-25 11:59:14 +0300

    Исправлено

--- Labs/Lab_2/1.cpp
+++ Labs/Lab_2/1.cpp
@@ -18,35 +18,69 @@ namespace Laba_2 {
 
             string str = "be,hair,help,maid,because,is,hair,she,is,jealous"; // если добавлю в конце строки слово is, то оно
             // становится повторяющимся, но почему-то не выводит на экран 
-            Console.WriteLine ( str );
-            Console.WriteLine ( "--------------------------------------------" );
-            string[] SplitStr = str.Split ( new Char[] { ' ', ',', '.', ':', '!', '?', '\t' } );//забиваем слово в массив, если встречается орпеделенный символ
-            Console.Write ( "Всего {0} слов(а) в строке\n", SplitStr.Length );
+            Console.WriteLine (str);
+            Console.WriteLine ("--------------------------------------------");
+            string [] SplitStr = str.Split (new Char [] { ' ', ',', '.', ':', '!', '?', '\t' });//забиваем слово в массив, если встречается орпеделенный символ
+            string [] arrTmp = new string [10]; 
+
+            Console.Write ("Всего {0} слов(а) в строке\n", SplitStr.Length);
             int MaxWordLength = 0;
-            foreach ( string s in SplitStr ) {
-                if ( s.Length > MaxWordLength ) { MaxWordLength = s.Length; }
+            foreach (string s in SplitStr) {
+                if (s.Length > MaxWordLength) {
+                    MaxWordLength = s.Length;
+                }
             }
             int countMax = 0;
-            foreach ( string s in SplitStr ) {
-                if ( s.Length == MaxWordLength ) { countMax++; }
+            int count = 0;
+            foreach (string s in SplitStr) {
+                if (s.Length == MaxWordLength) {
+                    countMax++;
+                }
             }
-            Console.Write ( "Всего {0} слов(а) имеют максимальную длинну\n", countMax );
+            Console.Write ("Всего {0} слов(а) имеют максимальную длинну\n", countMax);
 
-            Console.Write ( "Повторяющиеся слова:\n" );
+            Console.Write ("Повторяющиеся слова:\n");
             int StrLength = SplitStr.Length;
             int result = 0;
             countMax = 0;
-            for ( int i = 0; i < SplitStr.Length; i++ ) {
-                for ( int j = 0; j < SplitStr.Length; j++ ) {
-                    result = String.CompareOrdinal ( SplitStr[i], SplitStr[j] );
-                    if ( result == 0 ) {
+
+            for (int i = 0 ; i < SplitStr.Length ; i++) {
+                for (int j = 0 ; j < SplitStr.Length ; j++) {
+
+                    
+                    result = String.CompareOrdinal (SplitStr [i], SplitStr [j]);
+                   
+                    if (result == 0) {
                         countMax++;
-                        if ( countMax == 2 ) {
-                            Console.WriteLine ( "{0}", SplitStr[i] );
+                        if (countMax == 2) {
+                            for (int q = 0 ; q < arrTmp.Length ; q++) {
+                                if (arrTmp [q] != SplitStr [i]) {
+                                    arrTmp [q] = SplitStr [i];
+                                    count++;
+                                    break;
+                                }
+                            }
+                              
                         }
                     }
-                } //countMax = 0;
-            } 
+                }
+                countMax = 0;
+            }
+
+            for (int i = 0 ; i < arrTmp.Length ; i++) {
+                if (arrTmp [i] != null) {
+                    Console.WriteLine (arrTmp [i]);
+                }   
+            }
+                
+                
+
+
+
+
+
+
+
 
         }
     }
