16765ff6ef4ed9b760429bdce537c048d3443486
Anton Davedenko <davedenko83@gmail.com>
2015-03-31 22:58:10 +0300

    Переделано

--- Labs/Lab_5/2.cpp
+++ Labs/Lab_5/2.cpp
@@ -7,78 +7,141 @@ using System.Threading.Tasks;
 namespace _5._2 {
 
     class Fraction {
-        public double numerator;
-        public double denominator;
+        public double n;
+        public double d;
 
         public Fraction ( ) {
-            numerator = 0;
-            denominator = 0;
+            n = 0;
+            d = 0;
         }
         public Fraction ( int num, int den ) {
-            numerator = num;
-            denominator = den;
+            n = num;
+            d = den;
         }
         public static Fraction operator * ( Fraction f, int x ) {
+            //создаем времменую дробь         
             Fraction fract = new Fraction ( );
-            fract.numerator = f.numerator * x;
-            fract.denominator = f.denominator;
+            //перемножаем дроби и записываем во временную
+            fract.n = f.n * x;
+            fract.d = f.d;
+            // ищем наибольшее  и наименьшее значение дроби
+            double max = ( fract.n > fract.d ) ? fract.n : fract.d;
+            double min = ( fract.n < fract.d ) ? fract.n : fract.d;
+            // ищем НОК
+            double NOK =0 ;
+            do {
+                NOK+=max;
+            } while ( NOK % min!= 0 );
+            // ищем НОД
+            double NOD = ( min * max ) / NOK;
+            // сокращаем дробь
+            fract.n /= NOD;
+            fract.d /=NOD ;
             return fract;
         }
         public static Fraction operator * ( int x, Fraction f ) {
+            //создаем времменую дробь         
             Fraction fract = new Fraction ( );
-            fract.numerator = f.numerator * x;
-            fract.denominator = f.denominator;
+            //перемножаем дроби и записываем во временную
+            fract.n = f.n * x;
+            fract.d = f.d;
+            // ищем наибольшее  и наименьшее значение дроби
+            double max = ( fract.n > fract.d ) ? fract.n : fract.d;
+            double min = ( fract.n < fract.d ) ? fract.n : fract.d;
+            // ищем НОК
+            double NOK = 0;
+            do {
+                NOK += max;
+            } while ( NOK % min != 0 );
+            // ищем НОД
+            double NOD = ( min * max ) / NOK;
+            // сокращаем дробь
+            fract.n /= NOD;
+            fract.d /= NOD;
             return fract;
         }
+
 
         public static Fraction operator + ( Fraction f, double x ) {
             Fraction fract = new Fraction ( );
             double b = 0;
             double count = 0;
-            int a = 0;
-
+            //проверяем есть ли значения после запятой
+            int a = (int )x;
+            b = x - a;
+            //проверяем сколько значений после запятой
+            if ( b != 0 ) {
+                do {
+                    count++;
+                    b *= 10;
+                    a = ( int ) b;
+                    b = b - a;
+                } while ( b != 0 );
+            }
+            //Console.WriteLine ( "test count={0}",count );
+            //записываем значения в дробь
+            fract.n = x * ( 10 * count );
+            fract.d = Math.Pow ( 10, count );
+            //Console.WriteLine ( "num={0} den={1} ", fract.numerator, fract.denominator );
+            //ищем мин и макс для НОК 
+            double max =  fract.d > fract.n  ? fract.d : fract.n;
+            double min = fract.d < fract.n ? fract.d : fract.n;
+            //Console.WriteLine ( "max={0} min={1} ", max,min );
+            //ищем НОК
+            double NOK = 0;
             do {
-                b *= 10;
-                a = ( int ) x;
-                b = x - a;
-                count++;
-            } while ( b == 0 );
-
-            fract.numerator = x * ( 10 * count );//15
-            fract.denominator = Math.Pow ( 10, count );//10
-            double max = ( fract.denominator > f.denominator ) ? fract.denominator : f.denominator;
-            double min = ( fract.denominator < f.denominator ) ? fract.denominator : f.denominator;
-            b = max;
- 
+                NOK += max;
+            } while ( NOK % min != 0 );
+            //Console.WriteLine ( "NOK={0} ",NOK );
+            // ищем НОД
+            double NOD = ( min * max ) / NOK;
+            //Console.WriteLine ( "NOD={0} ", NOD );
+            // сокращаем дробь
+            fract.n /= NOD;
+            fract.d /= NOD;
+            //Console.WriteLine ( "fraction {0}/{1} ",fract.numerator,fract.denominator  );
+            //плюсуем дроби
+            //ищем мин и макс для НОК
+            max = f.d > fract.d ? f.d : fract.d;
+            min = f.d < fract.d ? f.d : fract.d;
+            //Console.WriteLine ( "max={0} min={1} ", max, min );
+            //ищем НОК
+            NOK = 0;
             do {
-                count = max % min;
-                max += b;
-            } while ( count == 0 );
-
-            fract.numerator = ( ( max / fract.denominator ) * fract.numerator ) + ( ( max / f.denominator ) * f.numerator );
-            fract.denominator = max;
+                NOK += max;
+            } while ( NOK % min != 0 );
+            //Console.WriteLine ( "NOK={0} ", NOK );
+            // ищем НОД
+            NOD = ( min * max ) / NOK;
+            //Console.WriteLine ( "NOD={0} ", NOD );
+            //записываем значения в дробь
+            fract.n = ( ( NOK / fract.d ) * fract.n ) + ( ( NOK / f.d ) * f.n );
+            fract.d = NOK;
             return fract;
         }
 
 
     }
- 
+
 
 
     class Program {
         static void Main ( string[] args ) {
-            Fraction f = new Fraction ( 3, 6 );//
-            int a = 10;
-            double d = 1.5;
+            Fraction f = new Fraction ( 3, 4 );
+            int a = 10;            
             Fraction f1 = f * a;
-            Console.WriteLine ( "f1={0}/{1} ", f1.numerator, f1.denominator );
+            Console.WriteLine ( "f1={0}/{1} ", f1.n, f1.d );
             Fraction f2 = a * f;
-            Console.WriteLine ( "f2={0}/{1} ", f2.numerator, f2.denominator );
+            Console.WriteLine ( "f2={0}/{1} ", f2.n, f2.d );
+            double d=1.5;
             Fraction f3 = f + d;
-            Console.WriteLine ( "f3={0}/{1} ", f3.numerator, f3.denominator );
-
-
+            Console.WriteLine ( "f3={0}/{1} ", f3.n, f3.d );
 
         }
+    
+    
+    
+    
+    
     }
 }
