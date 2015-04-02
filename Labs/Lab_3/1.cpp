8581944de70a93409bffa97753f53459929d04d7
Anton Davedenko <davedenko83@gmail.com>
2015-04-02 08:58:46 +0300

    готово

--- Labs/Lab_3/1.cpp
+++ Labs/Lab_3/1.cpp
@@ -13,7 +13,7 @@ namespace Laba_3 {
             /* 1.	Необходимо обернуть повторяющиеся слово в <strong> элемент
              * This test test is is  -->  This test<strong> test</strong> is<strong> is</strong>
              */
-            Console.WriteLine ("Введите строку:");
+        
             string str="This test test is is";
             string result=Regex.Replace (str, @"(\b(\w+?)\s)(\2)\b", "${1}<strong>${3}</strong>");
             Console.WriteLine (result);
