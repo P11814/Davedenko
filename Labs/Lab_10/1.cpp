242c9afa62fa8c73fe2158bdbbaa592602dc6c1c
Anton Davedenko <davedenko83@gmail.com>
2015-04-06 20:31:19 +0300

    не готово

--- Labs/Lab_10/1.cpp
+++ Labs/Lab_10/1.cpp
@@ -43,13 +43,179 @@ namespace Laba_10 {
             }
 
             StreamWriter fstr_out=new StreamWriter (fout);
-            fout.Close ();
+
+
+            int DArrHeight, DArrWidth, tmp;
+            int IArrHeight, IArrWidth;
+            string tmpStr;
+
+            Console.Write ("Введите свое имя: ");
+            tmpStr=Console.ReadLine ();
+            Console.Write ("Введите свою фамилию: ");
+            tmpStr+=" "+Console.ReadLine ();
+            Console.Write ("Введите своё отчество: ");
+            tmpStr+=" "+Console.ReadLine ();
+
+
+            Console.WriteLine ("\nВведите свою дату рождения");
+            Console.Write ("Число: ");
+            while (true) {
+                while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! "); }
+                if (tmp>0&&tmp<32) {
+                    break;
+                }
+                Console.Write ("Введите число ! ");
+            }
+            tmpStr+=" "+tmp;
+            //tmp=0;
+            Console.Write ("Месяц: ");
+            while (true) {
+                while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! "); }
+                if (tmp>0&&tmp<13) {
+                    break;
+                }
+                Console.Write ("Введите число ! ");
+            }
+            tmpStr+=" "+tmp;
+            //tmp=0;
+            Console.Write ("Год: ");
+            while (true) {
+                while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! "); }
+                if (tmp>1900&&tmp<2016) {
+                    break;
+                }
+                Console.Write ("Введите число ! ");
+            }
+            tmpStr+=" "+tmp+"\r\n"; // add newline ;
+
+
+            Console.Write ("\nВведите размер двумерного массива дробных чисел: ");
+            while (!int.TryParse (Console.ReadLine (), out DArrHeight)) { Console.WriteLine ("Введите число !"); }
+            tmpStr+=DArrHeight;
+            Console.Write ("Введите длину строки двумерного массива дробных чисел: ");
+            while (!int.TryParse (Console.ReadLine (), out DArrWidth)) { Console.WriteLine ("Введите число !"); }
+            tmpStr+=" "+DArrWidth+"\r\n";
+
+            double DTmp;
+            double[,] DArr=new double[DArrHeight, DArrWidth];
+
+            for (int i=0; i<DArrHeight; i++) {
+                for (int j=0; j<DArrWidth; j++) {
+                    Console.Write ("Введите значение : ");
+                    while (!double.TryParse (Console.ReadLine (), out DTmp)) { Console.Write ("Введите число ! - "); }
+                    DArr[i, j]=DTmp;
+                    tmpStr+=DArr[i, j]+" ";
+                }
+                tmpStr+="\r\n";
+            }
+
+
+            Console.Write ("\nВведите размер двумерного массива целых чисел: ");
+            while (!int.TryParse (Console.ReadLine (), out IArrHeight)) { Console.WriteLine ("Введите число !"); }
+            tmpStr+=IArrHeight;
+            Console.Write ("Введите длину строки двумерного массива целых чисел: ");
+            while (!int.TryParse (Console.ReadLine (), out IArrWidth)) { Console.WriteLine ("Введите число !"); }
+            tmpStr+=" "+IArrWidth+"\r\n";
+
+            int[,] IArr=new int[IArrHeight,IArrWidth];
+
+            for (int i=0; i<IArrHeight; i++) {
+                for (int j=0; j<IArrWidth; j++) {
+                    Console.Write ("Введите значение : ");
+                    while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! - "); }
+                    IArr[i, j]=tmp;
+                    tmpStr+=IArr[i, j]+" ";
+                }
+                
+            }
+            tmpStr+="\r\n";
+
+
+
+
+            int ok=0;
+            while (ok==0) {
+
+                string tmpDate=null;
+                Console.WriteLine ("\nВведите текущую дату");
+                Console.Write ("Число: ");
+                while (true) {
+                    while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! "); }
+                    if (tmp>0&&tmp<32) {
+                        break;
+                    }
+                    Console.Write ("Введите число ! ");
+                }
+                if (tmp<10) {
+                    tmpDate+="0"+tmp;
+                }
+                else {
+                    tmpDate+=tmp;
+                }
+                
+                //tmp=0;
+                Console.Write ("Месяц: ");
+                while (true) {
+                    while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! "); }
+                    if (tmp>0&&tmp<13) {
+                        break;
+                    }
+                    Console.Write ("Введите число ! ");
+                }
+                if (tmp<10) {
+                    tmpDate+=".0"+tmp;
+                }
+                else {
+                    tmpDate+="."+tmp;
+                }
+                
+                //tmp=0;
+                Console.Write ("Год: ");
+                while (true) {
+                    while (!int.TryParse (Console.ReadLine (), out tmp)) { Console.Write ("Введите число ! "); }
+                    if (tmp>1900&&tmp<2016) {
+                        break;
+                    }
+                    Console.Write ("Введите число ! ");
+                }
+                tmpDate+="."+tmp;
+
+                DateTime thisDay=DateTime.Today;
+
+                if (tmpDate==thisDay.ToString("d")) {
+                    tmpStr+=tmpDate;
+                    ok=1;
+                }
+
+            }
+
+
+
+            Console.WriteLine ();
+
+            try {
+                fstr_out.Write (tmpStr);
+            }
+            catch (IOException exc) {
+                Console.WriteLine (exc.Message+"File Error");
+                return;
+            }
+
+            fstr_out.Close ();
 
         }
 
+
+
+
+
+
+
+
+
         public static void openFile (string name) {
             FileStream fin;
-            
+            name+=".txt";
             try {
                 fin=new FileStream (name, FileMode.Open);
             }
@@ -69,6 +235,16 @@ namespace Laba_10 {
 
         }
 
+
+
+
+
+
+
+
+
+
+
         public static void changeDataFile (string name) {
             name+=".txt";
 
@@ -82,13 +258,18 @@ namespace Laba_10 {
 
         static void Main (string[] args) {
 
-            int ok=5;
+            
+
+
+
+            int ok=-1;
 
             while (ok!=0) {
 
                 Console.WriteLine (@"1. Создать файл
 2. Открыть и прочесть файл
-3. Записать форматированную информацию в файл");
+3. Записать форматированную информацию в файл
+0. Выход");
                 Console.WriteLine ();
                 Console.Write ("Введите число: ");
 
@@ -97,7 +278,7 @@ namespace Laba_10 {
                     ok=int.Parse (Console.ReadLine ());
                 }
                 catch (FormatException) {
-                    Console.WriteLine ();                   
+                    Console.WriteLine ();
                 }
                 catch (OverflowException) {
                     Console.WriteLine ();
@@ -144,7 +325,7 @@ namespace Laba_10 {
                     case 0:
                         ok=0;
                         break;
-
+                    
 
                 }
             }
